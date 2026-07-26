3f904e03330b00d2ce9f8d9fb373cb1521c883a4
Matthew Setter <matthew@matthewsetter.com>
2026-07-26 20:50:58 +1000

    Add documentation to the ReadingProficiency enum

--- src/Utilities/View/ReadingProficiency.php
+++ src/Utilities/View/ReadingProficiency.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Settermjd\MarkdownBlog\Utilities\View;
 
+/**
+ * This enum provides a simple representation of reading speeds.
+ *
+ * Based on https://wordstotime.com/, it provides values for three reading
+ * speeds:
+ *
+ * - "Slow" (100 wpm (words per minute))
+ * - "Average" (130 wpm)
+ * - "Fast" (160 wpm)
+ */
 enum ReadingProficiency
 {
     case Slow;
